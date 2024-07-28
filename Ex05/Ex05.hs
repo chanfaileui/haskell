bdac8028e6734acd64cccb1990db67bba1fcb850
Fai Chan <faileui@gmail.com>
2024-07-29 02:44:55 +1000

    complete

--- Ex05/Ex05.hs
+++ Ex05/Ex05.hs
@@ -4,6 +4,7 @@ import Control.Monad.List
 import Control.Monad.State (State, evalState, get, put)
 import Data.List
 import Debug.Trace
+import Test.QuickCheck
 
 -- Task 1. Eight Queens on a Chessboard
 
@@ -110,13 +111,13 @@ testExtend3 =
 testExtend4 :: Bool
 testExtend4 =
   let queens = [1, 3, 5]
-      expected = [[1, 3, 5, 0], [1, 3, 5, 2], [1, 3, 5, 7]] 
+      expected = [[1, 3, 5, 0], [1, 3, 5, 2], [1, 3, 5, 7]]
    in sort (extend queens) == sort expected
 
 testExtend5 :: Bool
 testExtend5 =
   let queens = [1, 3, 5, 0, 4]
-      expected = [] 
+      expected = []
    in sort (extend queens) == sort expected
 
 -- Aggregate all tests
@@ -131,8 +132,12 @@ allTestsPassed = and runTests
 --      returns the list of all possible solutions to the eight
 --      queens problem in which the queen in the first column
 --      is placed in the row position `p`.
+
+-- with reference to https://rosettacode.org/wiki/N-queens_problem#Haskell
 solutionStartWith :: Int -> [Queens]
-solutionStartWith = error "'solutionStartWith' not implemented"
+solutionStartWith p = foldM (\queens col -> extend queens) [p] [1 .. 7]
+
+-- for any n size: solutionStartWith n = foldM (\queens col -> extend queens) [] [0..n-1]
 
 -- Task 2. RPN calculator
 -- Reverse Polish notation (RPN) is a way of writing arithmetic
@@ -187,7 +192,20 @@ push x =
 --      the top element of the stack. Keep in mind that empty
 --      stacks are treated as if they were padded with zeroes.
 pop :: State Stack Double
-pop = error "'pop' not implemented"
+pop = do
+  xs <- get
+  case xs of
+    [] -> return 0
+    (x : xs') -> do
+      put xs'
+      return x
+
+-- pop :: State Stack Double
+-- pop =
+--   get >>= \xs ->
+--     case xs of
+--       [] -> return 0
+--       (x : xs') -> put xs' >> return x
 
 -- The Clear user action lets the user remove the top element
 -- of the current stack.
@@ -199,32 +217,49 @@ clear =
 -- 2.b. Implement the AllClear user action, which removes all
 --      elements from the stack.
 allClear :: State Stack ()
-allClear = error "'allClear' not implemented"
+allClear = get >>= \xs -> put []
 
 -- 2.c. Implement the Swap user action, which switches the order
 --      of the two topmost elements in the stack. Keep in mind
 --      that empty stacks are zero-padded.
 swap :: State Stack ()
-swap = error "'swap' not implemented"
+swap = do
+  x <- pop
+  y <- pop
+  push x
+  push y
 
 -- 2.d. Implement the `Arith` user action, which performs the
 --      given arithmetic operation on the top two elements of
 --      the stack. E.g. if the stack contains `[2,3]`, then
 --      `Arith (-)` should result in `[1]`.
 performArith :: (Double -> Double -> Double) -> State Stack ()
-performArith = error "'performArith' not implemented"
+performArith arith = do
+  x <- pop
+  y <- pop
+  push (arith y x)
 
 -- 2.e. Implement the `app1` function, which applies the given
 --      user action to the current stack.
 app1 :: UserAction -> State Stack ()
-app1 = error "'app1' not implemented"
+app1 useraction = do
+  case useraction of
+    Arith arith -> performArith arith
+    Clear -> clear
+    AllClear -> allClear
+    Swap -> swap
+    Enter x -> push x
 
 -- 2.f. Implement the `app` function, which applies the given
 --      list of user actions, in order, starting with the head
 --      of the list. Once all actions are perforemd, it returns
 --      the final state of the stack.
 app :: [UserAction] -> State Stack Stack
-app = error "'app' not implemented"
+app actions = do
+  mapM_ app1 actions
+  get
+
+-- foldM (\_ actions -> app1 actions) []
 
 -- you can use `runCalc` to test your implementation. E.g.
 
@@ -233,3 +268,106 @@ app = error "'app' not implemented"
 -- [6.0]
 runCalc :: [UserAction] -> Stack
 runCalc xs = evalState (app xs) []
+
+-- Property: Entering a number should put it on top of the stack
+prop_Enter :: Double -> Bool
+prop_Enter x = evalState (app [Enter x]) [] == [x]
+
+-- Property: Clear should remove the top element of the stack
+prop_Clear :: [Double] -> Bool
+prop_Clear xs = evalState (app [Clear]) xs == drop 1 xs
+
+-- Property: AllClear should empty the stack
+prop_AllClear :: [Double] -> Bool
+prop_AllClear xs = null (evalState (app [AllClear]) xs)
+
+-- Property: Swap should swap the top two elements of the stack
+prop_Swap :: Double -> Double -> Bool
+prop_Swap x y = evalState (app [Enter x, Enter y, Swap]) [] == [x, y]
+
+-- Property: Arith (+) should add the top two elements of the stack
+prop_Arith_Add :: Double -> Double -> Bool
+prop_Arith_Add x y = evalState (app [Enter x, Enter y, Arith (+)]) [] == [x + y]
+
+-- Property: Arith (-) should subtract the top element from the second top element
+prop_Arith_Sub :: Double -> Double -> Bool
+prop_Arith_Sub x y = evalState (app [Enter x, Enter y, Arith (-)]) [] == [x - y]
+
+-- Property: Arith (*) should multiply the top two elements of the stack
+prop_Arith_Mul :: Double -> Double -> Bool
+prop_Arith_Mul x y = evalState (app [Enter x, Enter y, Arith (*)]) [] == [x * y]
+
+-- Property: Arith (/) should divide the second top element by the top element of the stack
+prop_Arith_Div :: Double -> Double -> Property
+prop_Arith_Div x y = y /= 0 ==> evalState (app [Enter x, Enter y, Arith (/)]) [] == [x / y]
+
+-- Main function to run all QuickCheck properties
+main :: IO ()
+main = do
+  quickCheck prop_Enter
+  quickCheck prop_Clear
+  quickCheck prop_AllClear
+  quickCheck prop_Swap
+  quickCheck prop_Arith_Add
+  quickCheck prop_Arith_Sub
+  quickCheck prop_Arith_Mul
+  quickCheck prop_Arith_Div
+
+
+-- Test cases for the `app` function
+testApp1 :: Bool
+testApp1 =
+  let result = runCalc [Enter 3, Enter 7, Enter 5, Arith (-), Arith (*)]
+      expected = [6.0]
+   in result == expected
+
+testApp2 :: Bool
+testApp2 =
+  let result = runCalc [Enter 2, Enter 3, Arith (+), Enter 4, Arith (*)]
+      expected = [20.0]
+   in result == expected
+
+testApp3 :: Bool
+testApp3 =
+  let result = runCalc [Enter 10, Enter 4, Enter 3, Arith (-), Arith (/)]
+      expected = [10.0]
+   in trace (show ("testApp3", result)) (result == expected)
+
+testApp4 :: Bool
+testApp4 =
+  let result = runCalc [Enter 15, Clear, Enter 5, Enter 3, Arith (+)]
+      expected = [8.0]
+   in result == expected
+
+testApp5 :: Bool
+testApp5 =
+  let result = runCalc [Enter 1, Enter 2, Swap, Arith (/)]
+      expected = [2.0]
+   in result == expected
+
+testApp6 :: Bool
+testApp6 =
+  let result = runCalc [Enter 3, Enter 5, Enter 8, Enter 5, AllClear]
+      expected = []
+   in result == expected
+
+testApp7 :: Bool
+testApp7 =
+  let result = runCalc [Enter 3, Enter 5, Enter 8, AllClear, Enter 1]
+      expected = [1.0]
+   in result == expected
+
+-- Aggregate all tests
+runTestsAll :: [Bool]
+runTestsAll = [testApp1, testApp2, testApp3, testApp4, testApp5, testApp6, testApp7]
+
+-- Check if all tests passed
+allTestsPassedAll :: Bool
+allTestsPassedAll = and runTestsAll
+
+-- Main function to run all tests
+main1 :: IO ()
+main1 = do
+  if allTestsPassedAll
+    then putStrLn "All tests passed!"
+    else putStrLn "Some tests failed."
